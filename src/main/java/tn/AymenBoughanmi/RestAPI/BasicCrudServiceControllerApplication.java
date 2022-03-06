f466f4f0a9e633938f8fb1de68d449704b5a06f9
aymenBox <boxaymen7@gmail.com>
2022-03-06 03:59:03 +0100

    first commit

---
+++ src/main/java/tn/AymenBoughanmi/RestAPI/BasicCrudServiceControllerApplication.java
@@ -0,0 +1,13 @@
+package tn.AymenBoughanmi.RestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCrudServiceControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicCrudServiceControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/AymenBoughanmi/RestAPI/BasicCrudServiceControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.AymenBoughanmi.RestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCrudServiceControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
